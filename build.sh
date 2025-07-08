cf33d5c61f559d366bc3dae4daed777e8abbf75f
Here-we-start <massamba@mindabovecharts.com>
2025-07-08 21:14:51 +0200

    Fix build.sh to run npm commands from project directory

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ echo "Current directory: $(pwd)"
 echo "Listing files in current directory:"
 ls -la
 
+# Change to project directory where package.json is located
+cd project
+
 # Install dependencies
 echo "Installing dependencies..."
 npm install
